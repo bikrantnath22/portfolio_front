3b9dfab0814e94be5235c3edfcb95f6c8e4ac4dc
bikrantnath22 <bikrantnath4@gmail.com>
2025-03-02 22:40:20 +0530

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,12 +13,23 @@ export default function Header() {
   return (
     <nav className="fixed top-0 left-0 w-full bg-transparent  px-7 py-2 flex justify-between items-center z-50">
       {/* Left Side: Logo */}
-      <div className="text-white text-3xl font-mono font-bold mr-8 md:mr-0">
-        <Link to="/">Portfolio</Link>
+      <div className="text-white text-3xl font-mono font-bold mr-24 md:mr-0 justify-start">
+        <Link to="/">
+        <span className="text-green-400">P</span>
+        <span className="text-green-500">o</span>
+        <span className="text-green-50">r</span>
+        <span className="text-green-100">T</span>
+        <span className="text-green-200">f</span>
+        <span className="text-green-300">o</span>
+        <span className="text-green-400">L</span>
+        <span className="text-green-500">i</span>
+        <span className="text-green-600">o</span>
+        </Link>
+        
       </div>
 
       {/* Right Side: Navigation Links */}
-      <div className=" space-x-6 items-center hidden md:flex font-poppins">
+      <div className=" space-x-6 items-center hidden md:flex font-poppins text-[13px]">
         <Link to="/" className="flex items-center">
           <NavBtns
             icon={<HomeOutlined />}
